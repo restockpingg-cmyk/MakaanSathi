5b7d0e7c3239a1dbc76926c79b3cbd3e7a396fb6
Sarthak Barot <barotsarthak041@gmail.com>
2026-05-27 20:59:55 -0600

    Fix implicit any types in middleware cookie handlers

--- middleware.ts
+++ middleware.ts
@@ -10,15 +10,15 @@ export async function middleware(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name) {
+        get(name: string) {
           return request.cookies.get(name)?.value;
         },
-        set(name, value, options) {
+        set(name: string, value: string, options: object) {
           request.cookies.set({ name, value, ...options });
           response = NextResponse.next({ request: { headers: request.headers } });
           response.cookies.set({ name, value, ...options });
         },
-        remove(name, options) {
+        remove(name: string, options: object) {
           request.cookies.set({ name, value: '', ...options });
           response = NextResponse.next({ request: { headers: request.headers } });
           response.cookies.set({ name, value: '', ...options });
